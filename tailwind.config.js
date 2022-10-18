8ebbbcd3a57b2dfc8f71879f4b606c6c17cbb1c2
Nkaka23dev <nkakaeric96@gmail.com>
2022-10-18 20:36:45 +0200

    final commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,11 @@ module.exports = {
         secondray2: '#4d5156',
         darkBlue: '#1a0dab',
         muted: '#70757a',
-        bgLight:'#f2f2f2'
+        bgLight:'#f2f2f2',
+        khaki: '#e6e4e4d4'
       },
       fontFamily: {
-        font1:'Roboto,sans-serif',
+        fontRoboto:'Roboto,sans-serif',
       },
       boxShadow: {
         primary1: '1px 1.5px 1px 4px #f2f5f2'
@@ -34,7 +35,7 @@ module.exports = {
 				"80v": "80vh",
 				"90v": "90vh",
 				"100v": "100vh",
-        "5v": "5rem",
+        "5v": "5.5rem",
         "3v": "3rem"
 			}, 
       width: {
@@ -45,9 +46,33 @@ module.exports = {
       },
       fontSize: {
         '14/16': '.875rem',
-        'sm2': '0.8rem'
+        'sm2': '0.8rem',
+        '4': '4px',
+        '8': '8px'
       } 
     },
+    screens: {
+      'xs': '377px',
+        // => @media (min-width: 400px) { ... }
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+      'md2': '802px',
+      // => @media (min-width: 768px) { ... }
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+      'lg2': '1000px',
+      // => @media (min-width: 1024px) { ... }
+      'lg3': '1100px',
+      // => @media (min-width: 1024px) { ... }
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [],
 }
